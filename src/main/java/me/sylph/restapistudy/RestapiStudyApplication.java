3764f6f25b2db8811e08e1d4d82b982a7f3cb18c
sylph <sylph@sylphui-MacBookPro.local>
2023-02-15 22:56:46 +0900

    project creation

---
+++ src/main/java/me/sylph/restapistudy/RestapiStudyApplication.java
@@ -0,0 +1,13 @@
+package me.sylph.restapistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiStudyApplication.class, args);
+    }
+
+}
